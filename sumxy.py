51ea6a91ab473f4eddede9c59efdbd863a2fae8b
stevemburu <57958642+stevemburu@users.noreply.github.com>
2019-11-20 19:16:16 +0300

    First commit

---
+++ sumxy.py
@@ -0,0 +1,8 @@
+print('Please enter first number:')
+x = input()
+print('Please enter second number:')
+y = input()
+num1=int(x)
+num2 =int(y)
+sum = num1+num2
+print('Sum of the two numbers is:', sum)
